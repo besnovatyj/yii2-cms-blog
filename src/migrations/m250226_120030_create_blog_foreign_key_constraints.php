3d6b1f1d6617af9f6a63ebeca4c4fdb348857472
besnovatyj <metr31@yandex.ru>
2026-09-13 00:47:28 +0500

    upd: drop tags migrations

--- src/migrations/m250226_120030_create_blog_foreign_key_constraints.php
+++ src/migrations/m250226_120030_create_blog_foreign_key_constraints.php
@@ -49,20 +49,6 @@ class m250226_120030_create_blog_foreign_key_constraints extends BaseMigration
             'id',
             'CASCADE');
 
-        // Связь поста с тегами
-        $this->createFKs(
-            m250226_120020_create_blog_tag_asgmt_table::TABLE_NAME,
-            'post_id',
-            m250226_120010_create_blog_posts_table::TABLE_NAME,
-            'id',
-            'CASCADE');
-        $this->createFKs(
-            m250226_120020_create_blog_tag_asgmt_table::TABLE_NAME,
-            'tag_id',
-            m250226_120005_create_blog_tags_table::TABLE_NAME,
-            'id',
-            'CASCADE');
-
         // Связь поста с категориями
         $this->createFKs(
             m250226_120025_create_blog_taxonomy_asgmt_table::TABLE_NAME,
